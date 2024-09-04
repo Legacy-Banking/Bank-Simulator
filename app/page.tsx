13d3a71295b8fe09cc9524931a5e949a1e962cba
aidankahhoc <aidankahhoc@student.unimelb.edu.au>
2024-09-04 14:47:52 +1000

    Fix Login Page redirection

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ const Home = () => {
 
             {/* Buttons for Login & Signup*/}
             <div className="flex flex-col md:flex-row place-items-start space-x-0 space-y-4 md:space-x-10 md:space-y-0">
-              <Link href="/log-in">
+              <Link href="/login">
                 <button className="w-full md:w-auto px-6 md:px-8 py-2 md:py-3 text-base md:text-lg font-medium text-[#FFFFFF] bg-gradient-to-r from-[#468DC6] to-[#1A70B8] rounded-lg hover:text-gray-600 shadow-2xl">
                   → Login
                 </button>
