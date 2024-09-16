720aff8c5657c2c8fa0e3dbf2087599b36979735
Fwedwickk <fredrick@student.unimelb.edu.au>
2024-09-16 12:46:10 +1000

    Changed routes

--- components/AdminSide/AdminNavLink.tsx
+++ components/AdminSide/AdminNavLink.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 const AdminNavLink = ({ href, activePage, page, setActivePage, children }: { href?: string , activePage: string, page: string, setActivePage: (page: string) => void, children: React.ReactNode }) => {
     const isActive = activePage == page;
     
-    const unstyled = "font-semibold py-3 px-4 rounded-lg block mb-2 bg-white transition-colors duration-300 ease-in-out hover:bg-[rgb(255,231,163)]";
-    const styled = "font-semibold py-3 px-4 rounded-lg block mb-2 transition-all duration-300 bg-gradient-to-r from-[#f8bf5b] to-[rgb(255,231,163)]";
+    const unstyled = "font-semibold py-3 px-4 rounded-lg block mb-2 bg-white transition-colors duration-300 ease-in-out hover:bg-[rgb(255,231,163)] cursor-pointer";
+    const styled = "font-semibold py-3 px-4 rounded-lg block mb-2 transition-all duration-300 bg-gradient-to-r from-[#f8bf5b] to-[rgb(255,231,163)] cursor-pointer";
     const style = isActive ? styled : unstyled;
 
     const handleClick = (e: React.MouseEvent) => {
