d40ed98ed6d953423c197a093908143c18b5ac68
aidankahhoc <aidankahhoc@student.unimelb.edu.au>
2024-09-22 00:21:56 +1000

    [style] #167 Readjust font size of dashboard and Restyle accountbox

--- components/AccountBox.tsx
+++ components/AccountBox.tsx
@@ -18,8 +18,8 @@ const AccountBox: React.FC<AccountBoxProps> = ({ account }) => {
     const router = useRouter();
     const variant = account.type === 'savings' ? 'primary' : 'secondary';
     const boxStyle = variant === 'primary'
-        ? 'bg-yellow-gradient'
-        : 'bg-blue-gradient';
+        ? 'bg-gradient-to-r from-yellow-100 to-yellow-500'  // Lighter yellow to darker yellow
+        : 'bg-gradient-to-r from-[#4C97D1] to-[#1A70B8]';     // Lighter blue to darker blue
 
     const textColor = variant === 'primary'
         ? 'text-blackText-50 hover:text-white-100'
@@ -39,21 +39,21 @@ const AccountBox: React.FC<AccountBoxProps> = ({ account }) => {
             <div className='flex flex-col justify-between gap-4 rounded-lg border-y px-6 py-6 md:flex-row'>
                 <div className="flex flex-col gap-2">
                     <h2
-                        className={`text-24 lg:text-26 font-bold ${textColor} hover:underline cursor-pointer`}
+                        className={`text-24 lg:text-3xl font-bold ${textColor} hover:underline cursor-pointer`}
                         onClick={handleLinkClick}
                     >
                         {accountType} Account
                     </h2>
-                    <p className={`text-14`}>
+                    <p className={`text-lg`}>
                         {account?.owner_username}'s {account?.type} Account
                     </p>
-                    <p className="text-14 font-medium tracking-[1.1px] text-blackText-100">
+                    <p className="text-lg font-medium tracking-[1.1px] text-blackText-100">
                         BSB: {account?.bsb ? formatBSB(account.bsb.toString()) : ''} <span className="mx-4"> </span> Account Number: {account?.acc ? formatAccountNumber(account.acc.toString()) : ''}
                     </p>
                 </div>
 
                 <div className='flex-center flex-col gap-2 rounded-md bg-white-100/20 px-8 py-2 text-blackText-100 border border-black'>
-                    <div className="text-24 text-center font-bold">
+                    <div className="text-2xl text-center font-bold">
                         <BalanceBox
                             accounts={[]}
                             currentBalance={account.balance || 0}
