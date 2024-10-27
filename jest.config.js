a3c689629447588710232e41cafccb85d63dc73c
Fwedwickk <fredrick@student.unimelb.edu.au>
2024-10-28 00:22:36 +1100

    [refactor] #240 addde config files

--- jest.config.js
+++ jest.config.js
@@ -6,14 +6,28 @@ const createJestConfig = nextJest({
 })
 
 const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Ensure this points to the correct setup file
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/components/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
-    '^@/(.*)$': '<rootDir>/$1', // Resolve "@" alias
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js', 
+    '^@/(.*)$': '<rootDir>/$1',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
   testEnvironment: 'jest-environment-jsdom',
+  transformIgnorePatterns: [
+    'node_modules/(?!(query-string|decode-uri-component|split-on-first|filter-obj)/)'
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.json',
+      babelConfig: true,
+    }
+  }
 }
 
 module.exports = createJestConfig(customJestConfig)
--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,30 @@
 // jest.setup.js
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'
+
+// Mock next/router
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+      push: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn()
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null)
+    }
+  }
+}))
+
+// Mock next/image
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props) => {
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return <img {...props} />
+  }
+}))
