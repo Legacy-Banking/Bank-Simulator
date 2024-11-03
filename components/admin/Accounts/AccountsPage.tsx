8611bb70787c0f46d3bce60d3c9554e95e30fe35
Saite Guo <saiteguo@gmail.com>
2024-11-03 23:24:10 +1100

    [feat] #177 manual delete button

--- components/admin/Accounts/AccountsPage.tsx
+++ components/admin/Accounts/AccountsPage.tsx
@@ -21,7 +21,6 @@ const UsersPage = () => {
   const [page, setPage] = useState(1); // Initialize to page 1
 
   useEffect(() => {
-    // Access query parameters after component mounts
     if (typeof window !== 'undefined') {
       const params = new URLSearchParams(window.location.search);
       const pageParam = params.get('page');
@@ -32,8 +31,22 @@ const UsersPage = () => {
   }, []);
 
   const rowsPerPage = 10;
+  const listAndDeleteOldUsers = async () => {
+    try {
+      const oldUsers = await userAction.listOldUsers();
+      for (const user of oldUsers) {
+        await deleteUser(user.user_id);
+      }
+      fetchUsers();
+    } catch (error) {
+      console.error("Error listing or deleting old users:", error);
+    }
+  };
+  const deleteUser = async (userId: string) => {
+    const { error } = await supabase.auth.admin.deleteUser(userId);
+    if (error) throw new Error(error.message);
+  };
 
-  // Filter the accounts based on the input value
   const filteredUsers = accounts.filter((account) =>
     (account.owner_username ?? '').toLowerCase().includes(inputValue.toLowerCase())
   );
@@ -46,7 +59,6 @@ const UsersPage = () => {
   const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
   const currentUsers = uniqueUsers.slice(indexOfFirstTransaction, indexOfLastTransaction);
 
-  // Fetching data from Supabase
   const supabase = createClient();
   const fetchUsers = async () => {
     const { data, error } = await supabase.from('account').select('*');
@@ -59,14 +71,11 @@ const UsersPage = () => {
     }
     setLoading(false);
   };
+
   useEffect(() => {
-
     fetchUsers();
-  }, []); // Run once when the component mounts
+  }, []);
 
-
-
-  // Pop-up states
   const [showUpdatePopUp, setShowUpdatePopUp] = useState(false);
   const [showDeletePopUp, setShowDeletePopUp] = useState(false);
 
@@ -74,9 +83,9 @@ const UsersPage = () => {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <section 
-    data-testid={'accounts-page'}
-    className="flex w-full flex-row max-xl:max-h-screen font-inter">
+    <section
+      data-testid={'accounts-page'}
+      className="flex w-full flex-row max-xl:max-h-screen font-inter">
       <div className="flex w-full flex-1 flex-col gap-8 px-4 py-6 lg:py-12 lg:px-10 xl:px-20 2xl:px-32 xl:max-h-screen">
         <header className="home-header border-b pb-10">
           <HeaderBox
@@ -86,13 +95,18 @@ const UsersPage = () => {
           />
         </header>
 
-
         <div className='px-8 py-2'>
-          <div className='flex'>
+          <div className='flex items-center'>
             <div className='flex flex-1'>
               <h1 className="text-xl text-black font-semibold">Users</h1>
             </div>
             <SearchBar inputValue={inputValue} setInputValue={setInputValue} />
+            <button
+              onClick={listAndDeleteOldUsers}
+              className="ml-4 px-4 py-2 bg-red-500 text-white-100 rounded-md hover:bg-red-600"
+            >
+              Delete inactive Users
+            </button>
           </div>
           <section className="flex w-full flex-col mt-6 bg-white-100 rounded-b-3xl">
             <UsersTable
@@ -109,45 +123,39 @@ const UsersPage = () => {
             )}
           </section>
         </div>
-      {/* </div> */}
-      {showUpdatePopUp && (
-        <PopUp
-          message="Successfully Updated."
-          onClose={() => setShowUpdatePopUp(false)}
-        />
-      )}
-      {showDeletePopUp && (
-        <PopUp
-          message="Successfully Deleted."
-          onClose={() => setShowDeletePopUp(false)}
-        />
-      )}
-    <div className="bg-slate-400">
-     
-      {usersTest.map(user => {
-        // Find the account associated with this user by matching user_id with owner in accounts
-        const userAccount = accounts.find(account => account.owner === user.user_id);
 
-        return (
-          <option key={user.user_id} value={user.user_id}>
-            {userAccount ? (
-              <>
-                {userAccount.owner_username} - {user.last_sign_in_at}
-              </>
-            ) : (
-              <>
-                No account found for {user.user_id}
-              </>
-            )}
-          </option>
-        );
-      })}
-    </div>
+        {showUpdatePopUp && (
+          <PopUp
+            message="Successfully Updated."
+            onClose={() => setShowUpdatePopUp(false)}
+          />
+        )}
+        {showDeletePopUp && (
+          <PopUp
+            message="Successfully Deleted."
+            onClose={() => setShowDeletePopUp(false)}
+          />
+        )}
+        <div className="bg-slate-400">
+          {usersTest.map(user => {
+            const userAccount = accounts.find(account => account.owner === user.user_id);
 
-
-
-
-    </div>
+            return (
+              <option key={user.user_id} value={user.user_id}>
+                {userAccount ? (
+                  <>
+                    {userAccount.owner_username} - {user.last_sign_in_at}
+                  </>
+                ) : (
+                  <>
+                    No account found for {user.user_id}
+                  </>
+                )}
+              </option>
+            );
+          })}
+        </div>
+      </div>
     </section>
   )
 };
